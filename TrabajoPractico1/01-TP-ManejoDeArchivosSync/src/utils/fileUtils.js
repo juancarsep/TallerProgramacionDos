d41d37f08355bf214bf073075d646a37ffd65b29
U01-059-JUAN-C <carballosepejuan@gmail.com>
2024-04-12 10:30:43 -0300

    desarrollo de logica de los metodos de leer archivo y escribir archivo con fs

--- TrabajoPractico1/01-TP-ManejoDeArchivosSync/src/utils/fileUtils.js
+++ TrabajoPractico1/01-TP-ManejoDeArchivosSync/src/utils/fileUtils.js
@@ -1,9 +1,18 @@
+import fs from 'fs';
 /**
  * lee y devuelve el contenido de un archivo como texto en 'utf-8'
  * @param {string} ruta relativa al directorio del proyecto
  * @return {string} el texto leído
  */
 function leerArchivoComoString(ruta) {
+    let data = fs.readFile(ruta,'utf-8', (err, data) =>{
+        if(err){
+            console.log(`Error al leer el archivo: \n
+             ${err.message}`);
+        }else{
+            return data;
+        }
+    }) 
 }
 
 /**
@@ -12,6 +21,18 @@ function leerArchivoComoString(ruta) {
  * @param {string} texto 
  */
 function escribirTextoEnArchivo(ruta, texto, shouldCreateIfNotExists) {
+    fs.writeFile(ruta, texto, (err) => {
+        if(err){
+            console.log("Error al escribir en el archivo");
+        }else{
+            console.log("Se ha escrito en el archivo correctamente");
+        }
+    })
 }
 
 // exportar ambas funciones
+
+module.exports = {
+    leerArchivoComoString : leerArchivoComoString(),
+    escribirTextoEnArchivo : escribirTextoEnArchivo()
+}
