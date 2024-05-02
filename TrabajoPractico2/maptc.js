748390ed4a8bf3b8a9c8e4916ee5e4d039e979ba
U01-059-JUAN-C <carballosepejuan@gmail.com>
2024-05-02 15:36:26 -0300

    maptc terminado

---
+++ TrabajoPractico2/maptc.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+
+
+function contenidoStr(archivoALeer){
+    return new Promise((resolve, reject) => {
+        let contenidoStr = fs.readFile(archivoALeer, 'utf-8', (error, data) =>{
+            if(error){
+                reject(error);
+            }else{
+                resolve(data.toString);
+            }
+        })
+    })
+}
+
+
+function contenidoObj(archivoALeer){
+    return new Promise((resolve, reject) => {
+        let contenidoStr = fs.readFile(archivoALeer, 'utf-8', (error, data) =>{
+            if(error){
+                reject(error);
+            }else{
+                resolve(JSON.parse(data));
+            }
+        })
+    })
+}
+
+function sizeArchivo(archivoALeer){
+    return new Promise((resolve, reject) =>{
+        let size = fs.stat(archivoALeer, (error, data) =>{
+            if(error){
+                reject(error);
+            }else{
+                resolve(data.size);
+            }
+        })
+    })
+}
+
+let info = {
+    contenidoStr : contenidoStr('package.json'),
+    contenidoObj : contenidoObj('package.json'),
+    size : sizeArchivo('package.json')
+}
+
+function escribirArchivo(archivoAEscribir, textoAEscribir){
+    return new Promise((reject, resolve) =>{
+        fs.writeFile(archivoAEscribir,textoAEscribir, (error, data) =>{
+            if(error){
+                reject(error);
+            }else{
+                resolve();
+            }
+        })
+    })
+}
